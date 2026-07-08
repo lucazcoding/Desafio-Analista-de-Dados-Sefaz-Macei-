0199d3720626b731924a84babd097d2da8eca586
lucazcoding <lucazcode@gmail.com>
2026-07-07 22:57:39 -0300

    atualizando caminho parquet

--- src/utils/constantes.py
+++ src/utils/constantes.py
@@ -4,5 +4,5 @@ DIRETORIO_RAIZ = Path(__file__).parent.parent.parent
 
 PASTA_COMPACTOS = DIRETORIO_RAIZ / "dados_compactos"
 PASTA_EXTRAIDOS = DIRETORIO_RAIZ / "dados_extraidos"
-CAMINHO_PARQUET = PASTA_EXTRAIDOS / "finbra_consolidado.parquet"
+CAMINHO_PARQUET = Path("data/processed/finbra_consolidado.parquet")
 CAMINHO_DUCKDB = DIRETORIO_RAIZ / "finbra.duckdb"
